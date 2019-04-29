81800bec4a7a903c2e58d072160ec6f9b039db09
KonradGasior <konrad.andrzej.gasior@gmail.com>
2019-04-29 22:14:26 +0100

    Written basic skeleton of my pokedex app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-alert('Hello world!')
+// Definition of the pokemon object
+var pokemon = {
+  name,
+  heigth,
+  types: ["Electric", "Fire", "Water"]
+}
+// Initialized 3 objects
+pokemon = {"pikachu", 0.4, types[1]}
+pokemon = {"charmander", 0.6, types[2]}
+pokemon = {"squirtle", 0.5, types[3]}
+// Declared variables for the loop
+var i;
+var repository[i] = [pokemon];
