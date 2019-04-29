c5ab1a18517add8f30b18a212aa1e4c560e8828c
KonradGasior <konrad.andrzej.gasior@gmail.com>
2019-04-29 23:00:48 +0100

    Reworked object declaration
    Created 3 separate objects for each pokemon, created repository variable holding each object in an array, also written an loop which print all of the metioned above.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-// Definition of the pokemon object
-var pokemon = {
-  name,
-  heigth,
-  types: ["Electric", "Fire", "Water"]
+// Definitions of the pokemon object
+var pokemon1 = {
+  name: "Pikachu",
+  heigth: 0.4,
+  types: ["Electric"]
+};
+
+var pokemon2 = {
+  name: "Charmander",
+  heigth: 0.6,
+  types: ["Fire"]
+};
+
+var pokemon3 = {
+  name: "Squirtle",
+  heigth: 0.5,
+  types: ["Water"]
+};
+
+// Defined repository variable containing all of the objects in an array
+var repository = [ pokemon1, pokemon2, pokemon3 ];
+
+// Loop which will print the content of the pokemon object
+for ( var i = 0; i < 3; i++ ) {
+  if (repository[i].heigth > 0.5) {
+    document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth +" \) -- Wow that's big!!! </p>" );
+  } else {
+      document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth +" \)</p>" ); }
 }
-// Initialized 3 objects
-pokemon = {"pikachu", 0.4, types[1]}
-pokemon = {"charmander", 0.6, types[2]}
-pokemon = {"squirtle", 0.5, types[3]}
-// Declared variables for the loop
-var i;
-var repository[i] = [pokemon];
