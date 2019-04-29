dd6803cc38c6aaee77883d6e676514cc03834f86
KonradGasior <konrad.andrzej.gasior@gmail.com>
2019-04-30 00:14:29 +0100

    style changes
    Added type for each pokemon and rule which determined class based on type of pokemon and set proper color in CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,28 @@ var repository = [ pokemon1, pokemon2, pokemon3 ];
 // Loop which will print the content of the pokemon object
 for ( var i = 0; i < 3; i++ ) {
   if (repository[i].heigth > 0.5) {
-    document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth +" \) -- Wow that's big!!! </p>" );
-  } else {
-      document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth +" \)</p>" ); }
+
+    // this line will add extra text only if pokemon heigth excedes 0.5m.
+    document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth + ", Type: ")
+
+    if (repository[i].types[0] === "Fire") {
+      document.write("<span class = \"fire\">" + repository[i].types[0] + "</span>")
+    } else if(repository[i].types[0] === "Water") {
+      document.write("<span class = \"water\">" + repository[i].types[0] + "</span>")
+    } else if(repository[i].types[0] === "Electric") {
+      document.write("<span class = \"electric\">" + repository[i].types[0] + "</span>")}
+
+    document.write(" \) -- Wow that's big!!! </p>" )
+  } else  {
+
+    document.write("<p>" + repository[i].name + "\( " + "heigth: " + repository[i].heigth + ", Type: " )
+
+    // this part change color of the pokemon based on type
+    if (repository[i].types[0] === "Fire") {
+      document.write("<span class = \"fire\">" + repository[i].types[0] + "</span> " + " \)" + "</p>")
+    } else if(repository[i].types[0] === "Water") {
+      document.write("<span class = \"water\">" + repository[i].types[0] + "</span> " + " \)" + " </p>")
+    } else if(repository[i].types[0] === "Electric") {
+      document.write("<span class = \"electric\">" + repository[i].types[0] + "</span> " + " \)" + "</p>") }
+    }
 }
