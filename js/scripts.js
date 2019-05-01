d523405beb444332ec808911c6e8a0a8a09a419d
KonradGasior <konrad.andrzej.gasior@gmail.com>
2019-05-01 22:08:37 +0100

    Functional programing update
    Added if statement to check if data type is an object. Replaced for loop with forEach function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@
 var pokedexApp = (function (){
 
   // IIFE for pokemon repository variable
-  pokemonRepository(function() {
+  var pokemonRepository = (function() {
     var repository = [];
 
     function add (item) {
-      repository.push(item);
+      if(typeof item === 'object'){
+        repository.push(item);}
     };
 
     function getAll () {
@@ -15,10 +16,10 @@ var pokedexApp = (function (){
 
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
     };
   })();
-  
+
   // Definitions of the pokemon object
   var pokemon1 = {
     name: "Pikachu",
@@ -38,33 +39,34 @@ var pokedexApp = (function (){
     types: ["Water"]
   };
 
+  pokemonRepository.add(pokemon1);
+  pokemonRepository.add(pokemon2);
+  pokemonRepository.add(pokemon3);
+
   // Defined repository variable containing all of the objects in an array
 
-  var top_value = repository[0].heigth;
+  var top_value = pokemonRepository.getAll()[0].heigth;
 
   // Loop which will print the content of the pokemon object
-  for ( var i = 0; i < 3; i++ ) {
-
+  pokemonRepository.getAll().forEach (function(pokemon){
     // this code will add extra text to the highest pokemon.
-    if (repository[i].heigth > top_value) {
-      top_value = repository[i].heigth;
-      pokemonWrite(repository[i])
+    if (pokemon.heigth > top_value) {
+      top_value = pokemon.heigth;
+      pokemonWrite(pokemon)
       document.write(" -- Wow that's big!!! " )
     } else  {
-      pokemonWrite(repository[i])
-    }
-  };
+        pokemonWrite(pokemon)}
+  });
 
   function pokemonWrite (pokemon) {
     // this function outoputs pokemon data and change color of the font based on type of pokemon.
     document.write("<p>" + pokemon.name + "\( " + "heigth: " + pokemon.heigth + ", Type: " )
     if (pokemon.types[0] === "Fire") {
-      document.write("<span class = \"fire\">" + pokemon.types[0] + "</span> " + " \)")
+        document.write("<span class = \"fire\">" + pokemon.types[0] + "</span> " + " \)")
     } else if(pokemon.types[0] === "Water") {
-      document.write("<span class = \"water\">" + pokemon.types[0] + "</span> " + " \)")
+        document.write("<span class = \"water\">" + pokemon.types[0] + "</span> " + " \)")
     } else if(pokemon.types[0] === "Electric") {
-      document.write("<span class = \"electric\">" + pokemon.types[0] + "</span> " + " \)")}
-    }
-
+        document.write("<span class = \"electric\">" + pokemon.types[0] + "</span> " + " \)")}
+    };
 // End of IIFE pokedexApp.
 })();
