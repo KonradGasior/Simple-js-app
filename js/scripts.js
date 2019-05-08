d07a34dc51e0d5722adafaf38a8e6c8afe86cee1
KonradGasior <konrad.andrzej.gasior@gmail.com>
2019-05-08 23:20:36 +0100

    DOM manipulation update.
    Created html app skeleton and basic layout for the app and it's UI, added normalize.css rules to improve consistency in app display across different browsers. Removed function pokemonWrite and replaced it with pokemonRepository method addListItem to create an list of buttons with pokemon name taken from object parameter. Minor: added google font.

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,22 @@ var pokedexApp = (function (){
       return repository;
     };
 
+
+    //This function creat an nodes in DOM to create list of pokemons.
+    function addListItem (item) {
+      var $listItem = document.createElement('LI');
+      var $button = document.createElement('Input');
+      var $list = document.querySelector('.pokedex-window');
+      $list.appendChild($listItem);
+      $listItem.appendChild($button);
+      $button.setAttribute('type', 'button');
+      $button.setAttribute('value', item.name)
+    }
+
     return {
       add: add,
       getAll: getAll,
+      addListItem: addListItem
     };
   })();
 
@@ -40,33 +53,9 @@ var pokedexApp = (function (){
   };
 
   pokemonRepository.add(pokemon1);
-  pokemonRepository.add(pokemon2);
-  pokemonRepository.add(pokemon3);
+  pokemonRepository.addListItem(pokemon2);
+  pokemonRepository.addListItem(pokemon3);
 
-  // Defined repository variable containing all of the objects in an array
 
-  var top_value = pokemonRepository.getAll()[0].heigth;
-
-  // Loop which will print the content of the pokemon object
-  pokemonRepository.getAll().forEach (function(pokemon){
-    // this code will add extra text to the highest pokemon.
-    if (pokemon.heigth > top_value) {
-      top_value = pokemon.heigth;
-      pokemonWrite(pokemon)
-      document.write(" -- Wow that's big!!! " )
-    } else  {
-        pokemonWrite(pokemon)}
-  });
-
-  function pokemonWrite (pokemon) {
-    // this function outoputs pokemon data and change color of the font based on type of pokemon.
-    document.write("<p>" + pokemon.name + "\( " + "heigth: " + pokemon.heigth + ", Type: " )
-    if (pokemon.types[0] === "Fire") {
-        document.write("<span class = \"fire\">" + pokemon.types[0] + "</span> " + " \)")
-    } else if(pokemon.types[0] === "Water") {
-        document.write("<span class = \"water\">" + pokemon.types[0] + "</span> " + " \)")
-    } else if(pokemon.types[0] === "Electric") {
-        document.write("<span class = \"electric\">" + pokemon.types[0] + "</span> " + " \)")}
-    };
 // End of IIFE pokedexApp.
 })();
